2e5a0330490869507d076aaeb1af04931ba7125c
pdolega <pdolega@gmail.com>
2019-10-18 22:38:45 +0200

    Removing MathContext

--- src/main/scala/com/virtuslab/scalaworkshop/intro/ScalaFeel.scala
+++ src/main/scala/com/virtuslab/scalaworkshop/intro/ScalaFeel.scala
@@ -1,17 +1,15 @@
 package com.virtuslab.scalaworkshop.intro
 
-import java.math.MathContext
-
 object ScalaFeel {
   case class Car(brand: String, model: String, engine: BigDecimal, clazz: String)
 
   def main(args: Array[String]): Unit = {
     val cars = List(
-      new Car("Fiat", "500", BigDecimal("1.0", MathContext.DECIMAL32), "C"),
-      new Car("Audi", "A4", BigDecimal("1.6", MathContext.DECIMAL32), "C"),
-      new Car("Ford", "Focus", BigDecimal("1.8", MathContext.DECIMAL32), "C"),
-      new Car("BMW", "1", BigDecimal("2.0", MathContext.DECIMAL32), "C"),
-      new Car("Ford", "Focus", BigDecimal("1.8", MathContext.DECIMAL32), "C")
+      new Car("Fiat", "500", BigDecimal("1.0"), "C"),
+      new Car("Audi", "A4", BigDecimal("1.6"), "C"),
+      new Car("Ford", "Focus", BigDecimal("1.8"), "C"),
+      new Car("BMW", "1", BigDecimal("2.0"), "C"),
+      new Car("Ford", "Focus", BigDecimal("1.8"), "C")
     )
     //
   }
